fc309d9d349d687dd929426a792a11fb7a6f5ad0
Karthick <36299082+karthick4pm@users.noreply.github.com>
2024-01-03 22:45:53 -0500

    Add files via upload

---
+++ student-services-security/src/main/java/com/in28minutes/springboot/StudentServicesSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesSecurityApplication.class, args);
+	}
+
+}
---
+++ student-services-security/src/main/java/com/in28minutes/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.springboot.model.Employee;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
